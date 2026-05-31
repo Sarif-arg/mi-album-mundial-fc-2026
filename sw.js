4db271c41a48d750497556fe5295b75c72fec595
FEDE <federicosarif@gmail.com>
2026-05-31 20:09:16 -0300

    Updates

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'album-mundial-2026-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Only intercept HTTP/HTTPS queries (ignore chrome-extension, etc.)
+  if (e.request.url.startsWith('http')) {
+    e.respondWith(
+      caches.match(e.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(e.request);
+      })
+    );
+  }
+});
